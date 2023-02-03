2784ba162414e49df402eb5b032ebf3bdb7b6197
amartya061997@gmail.com <amartya061997@gmail.com>
2023-02-03 16:55:29 +0530

    commit

--- Http.js
+++ Http.js
@@ -1,29 +1,38 @@
 const request = require('request')
- 
-const url = 'http://api.weatherstack.com/current?access_key=7ac764fbde97a5f29b7a63a3dac9a1fe&query=kolkata'
- 
-request({ url: url }, (error, response) => {
+
+const temparature=(data,callback)=>{
+  const url = 'http://api.weatherstack.com/current?access_key=7ac764fbde97a5f29b7a63a3dac9a1fe&query='+data.latitude+', '+data.longitude
+  request({ url: url }, (error, response) => {
     if (error) {
         console.log('Unable to connect the server!')
     } else {
          const data=JSON.parse(response.body)
-    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress.')
-    console.log('Today is Tuesday and it is '+data.current.temperature+'degree temperature ')
+          callback(data)
     }
+  })
+}
+
+const geoURLfunction=(location,callback)=>{
+  const geoURL='https://api.geoapify.com/v1/geocode/search?text='+location+'&format=json&apiKey=d548c5ed24604be6a9dd0d989631f783'
+
+    request({url:geoURL,json:true},(error,response)=>{
+      // console.log(response.body.data)
+    if (error) {
+      console.log('Unable to connect the server!')
+    } else {
+      const data={
+        longitude:response.body.results[0].lon,
+        latitude:response.body.results[0].lat
+      }
+    callback(data)
+    }
+  })
+}
+
+geoURLfunction('kolkata, West Bengal, India',(data)=>{
+  temparature(data,(data)=>{
+    console.log('Amartya')
+
+    console.log('Currently it is '+data.current.weather_descriptions[0]+ ' and the temparature' +data.temparature+' degree celcius.')
+  })
 })
-
-
-const geoURL='http://api.positionstack.com/v1/forward?access_key=f2682f0b98926b2f5d27c9587b8c10e8&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC'
-
-request({url:geoURL,json:true},(error,response)=>{
-    // console.log(response.body.data)
-  if (error) {
-    console.log('Unable to connect the server!')
-  } else {
-    const latitude=response.body.data[0].latitude
-    console.log(latitude)
-    const longitude=response.body.data[1].longitude
-     console.log(longitude)
-  }
-})
-
--- callback.js
+++ callback.js
@@ -1,18 +1,38 @@
-setTimeout(() => {
-    console.log('Two seconds')
-}, 2000);
+// setTimeout(() => {
+//     console.log('Two seconds')
+// }, 2000);
 
-const names=['Amartya','Avirup','Pratap','Saswati']
+// const names=['Amartya','Avirup','Pratap','Saswati']
 
-const shortNames=names.filter((name)=>{
-    return name.length>=4
-})
+// const shortNames=names.filter((name)=>{
+//     return name.length>=7
+// })
 
-console.log(shortNames)
+// console.log(shortNames)
 
-const geoCode=(address,callback)=>{
-    const data={
-        latitude:0,
-        longitude:0
-    }
+// const geoCode=(address,callback)=>{
+//     setTimeout(() => {
+//         const data={
+//             latitude:0,
+//             longitude:0
+//         }
+//         callback(data)
+//     }, 2000);
+// }
+
+// geoCode('Kolkata',(data)=>{
+//     console.log(data)
+// })
+
+
+const add=(a,b,callback)=>{
+    setTimeout(() => {
+        const data=a+b
+
+        callback(data)
+    }, 2000);
 }
+
+add(1,4,(sum)=>{
+    console.log(sum)
+})
