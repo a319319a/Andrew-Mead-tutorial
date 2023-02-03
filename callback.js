2240d7fa57d6ab26de02c9d39dd57fd0cdadcf63
amartya061997@gmail.com <amartya061997@gmail.com>
2023-02-03 17:14:39 +0530

    commit

--- callback.js
+++ callback.js
@@ -1,28 +1,28 @@
-// setTimeout(() => {
-//     console.log('Two seconds')
-// }, 2000);
+setTimeout(() => {
+    console.log('Two seconds')
+}, 2000);
 
-// const names=['Amartya','Avirup','Pratap','Saswati']
+const names=['Amartya','Avirup','Pratap','Saswati']
 
-// const shortNames=names.filter((name)=>{
-//     return name.length>=7
-// })
+const shortNames=names.filter((name)=>{
+    return name.length>=7
+})
 
-// console.log(shortNames)
+console.log(shortNames)
 
-// const geoCode=(address,callback)=>{
-//     setTimeout(() => {
-//         const data={
-//             latitude:0,
-//             longitude:0
-//         }
-//         callback(data)
-//     }, 2000);
-// }
+const geoCode=(address,callback)=>{
+    setTimeout(() => {
+        const data={
+            latitude:0,
+            longitude:0
+        }
+        callback(data)
+    }, 2000);
+}
 
-// geoCode('Kolkata',(data)=>{
-//     console.log(data)
-// })
+geoCode('Kolkata',(data)=>{
+    console.log(data)
+})
 
 
 const add=(a,b,callback)=>{
